4715b5add8661f5cccb5a9144e9d9b4cf6112026
YU-SIANG,CHEN <qbb225827455@gmail.com>
2022-08-09 22:36:37 +0800

    Use JSONDecoder

--- JSONdecoderDemo/Loan.swift
+++ JSONdecoderDemo/Loan.swift
@@ -7,9 +7,36 @@
 
 import Foundation
 
-struct Loan: Hashable {
+struct Loan: Hashable, Codable {
     var name: String = ""
-    var country: String = ""
     var use: String = ""
+    var country: String = ""
     var amount: Int = 0
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case use
+        case country = "location"
+        case amount = "loan_amount"
+    }
+    
+    enum LoacationKeys: String, CodingKey {
+        case country
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        name = try values.decode(String.self, forKey: .name)
+        use = try values.decode(String.self, forKey: .use)
+        amount = try values.decode(Int.self, forKey: .amount)
+        
+        let location = try values.nestedContainer(keyedBy: LoacationKeys.self, forKey: .country)
+        country = try location.decode(String.self, forKey: .country)
+    }
+}
+
+struct LoanDataStore: Codable {
+    var loans: [Loan]
 }
--- JSONdecoderDemo/TableViewController.swift
+++ JSONdecoderDemo/TableViewController.swift
@@ -64,23 +64,11 @@ class TableViewController: UITableViewController {
         
         var loans = [Loan]()
         
+        let decoder = JSONDecoder()
+        
         do {
-            let jsonResult = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
-            
-            let jsonLoans = jsonResult?["loans"] as! [AnyObject]
-            for jsonLoan in jsonLoans {
-                
-                var loan = Loan()
-                
-                loan.name = jsonLoan["name"] as! String
-                loan.use = jsonLoan["use"] as! String
-                loan.amount = jsonLoan["loan_amount"] as! Int
-                
-                let location = jsonLoan["location"] as! [String:AnyObject]
-                loan.country = location["country"] as! String
-                
-                loans.append(loan)
-            }
+            let loanDataStore = try decoder.decode(LoanDataStore.self, from: data)
+            loans = loanDataStore.loans
             
         } catch {
             print(error)
