bef996cdc9665f4e54462cf9efc56caf270d43a7
YU-SIANG,CHEN <qbb225827455@gmail.com>
2022-08-09 20:12:40 +0800

    no message

---
+++ JSONdecoderDemo/Loan.swift
@@ -0,0 +1,15 @@
+//
+//  Loan.swift
+//  JSONdecoderDemo
+//
+//  Created by 陳鈺翔 on 2022/8/9.
+//
+
+import Foundation
+
+struct Loan: Hashable {
+    var name: String = ""
+    var country: String = ""
+    var use: String = ""
+    var amount: Int = 0
+}
--- JSONdecoderDemo/TableViewCell.swift
+++ JSONdecoderDemo/TableViewCell.swift
@@ -9,6 +9,23 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
 
+    @IBOutlet weak var nameLabel:UILabel! {
+        didSet {
+            nameLabel.numberOfLines = 0
+        }
+    }
+    @IBOutlet weak var countryLabel:UILabel! {
+        didSet {
+            countryLabel.numberOfLines = 0
+        }
+    }
+    @IBOutlet weak var useLabel:UILabel! {
+        didSet {
+            useLabel.numberOfLines = 0
+        }
+    }
+    @IBOutlet weak var amountLabel:UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
