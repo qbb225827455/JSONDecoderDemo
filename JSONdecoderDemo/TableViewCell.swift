4592cb964b9a606d9c2de4655b41ebd3e55fff4a
YU-SIANG,CHEN <qbb225827455@gmail.com>
2022-08-09 16:55:42 +0800

    Storyboard init

---
+++ JSONdecoderDemo/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  JSONdecoderDemo
+//
+//  Created by 陳鈺翔 on 2022/8/9.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
